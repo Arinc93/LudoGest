5be6fd0db24b401d805935cda9faef336def728d
Arinc93 <arinc93@gmail.com>
2017-12-15 20:16:13 +0100

    aun no funciona la lista

--- LudoGest/app/src/main/java/com/example/dominpc/ludogest/View/apuestasShowAll.java
+++ LudoGest/app/src/main/java/com/example/dominpc/ludogest/View/apuestasShowAll.java
@@ -2,7 +2,10 @@ package com.example.dominpc.ludogest.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
+import com.example.dominpc.ludogest.Core.DBManager;
 import com.example.dominpc.ludogest.R;
 
 public class apuestasShowAll extends AppCompatActivity {
@@ -11,5 +14,33 @@ public class apuestasShowAll extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apuestas_show_all);
+
+        final ListView listApuestas = this.findViewById( R.id.lvApuestas );
+
+        this.registerForContextMenu( listApuestas );
+        DBManager DBMgr = new DBManager( this.getApplicationContext() );
+
+
+    }
+    protected void onResume() {
+        super.onResume();
+        final ListView listApuestas = this.findViewById( R.id.lvApuestas );
+
+
+        SimpleCursorAdapter mainCursorAdapter = new SimpleCursorAdapter( this,
+                R.layout.lvlistaapuestas,
+                null,
+                new String[]{ DBManager.APUESTA_ID ,DBManager.APUESTA_EVENTO },
+                new int[] { R.id.lvidApuesta, R.id.lveventoApuesta } );
+        listApuestas.setAdapter( mainCursorAdapter );
+
+
+
+
+    }
+
+    private void updateContacts()
+    {
+        this.mainCursorAdapter.changeCursor( this.dbManager.getAllContacts() );
     }
 }
